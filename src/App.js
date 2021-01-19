69b40db94a29d5adaf947af1e72c0143c081121b
Romie Hecdivert <65568062+rh9891@users.noreply.github.com>
2021-01-18 23:42:15 -0500

    Created a Header component for the application. Application working as expected.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import { Header } from "./components/Header";
 
 function App() {
-  return <div>Dime and Nickel</div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 }
 
 export default App;
