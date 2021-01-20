72887eefde409b03c53aa4bc1bc777482ad0eebd
Romie Hecdivert <65568062+rh9891@users.noreply.github.com>
2021-01-20 14:39:20 -0500

    Built out the balance component of the UI. Application is working as expected.

---
+++ src/components/Balance.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const Balance = () => {
+  return (
+    <>
+      <h4 className="subtitle-balance">Current Balance</h4>
+      <h1 className="balance" id="balance">
+        $0.00
+      </h1>
+    </>
+  );
+};
