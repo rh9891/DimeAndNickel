1c06a0f3f082e25ada011c9102c54de78b346931
Romie Hecdivert <65568062+rh9891@users.noreply.github.com>
2021-01-20 16:00:38 -0500

    Added a global provider to wrap the components in a state to have access to the global state. Created and brought in a reducer. Application is working as expected.

---
+++ src/context/AppReducer.js
@@ -0,0 +1,7 @@
+// Specifies the application's state changes in response to certain actions to our context.
+export default (state, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
---
+++ src/context/GlobalState.js
@@ -0,0 +1,26 @@
+import React, { createContext, useReducer } from "react";
+import AppReducer from "./AppReducer";
+
+// Initial state.
+const initialState = {
+  transactions: [
+    { id: 1, text: "Flowers", amount: -20 },
+    { id: 2, text: "Salary", amount: 300 },
+    { id: 3, text: "Smoothie", amount: -10 },
+    { id: 4, text: "Tickets", amount: 150 },
+  ],
+};
+
+// Creates context.
+export const GlobalContext = createContext(initialState);
+
+// Global provider component (wraps all other components in a state so that they have access to the global state).
+export const GlobalProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(AppReducer, initialState);
+
+  return (
+    <GlobalContext.Provider value={{ transactions: state.transactions }}>
+      {children}
+    </GlobalContext.Provider>
+  );
+};
