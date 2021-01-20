d2ad5be99676573ca3d9087b168ba2010e10fedc
Romie Hecdivert <65568062+rh9891@users.noreply.github.com>
2021-01-20 15:23:10 -0500

    Modified the AddTransaction component so that there are two pieces of state and an onChange event to find the change in state. Basic UI of application is completed.

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,17 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const AddTransaction = () => {
+  const [text, setText] = useState("");
+  const [amount, setAmount] = useState(0);
+
   return (
     <>
       <h3>Add Transaction</h3>
       <form>
         <div className="form-control">
           <label htmlFor="text">Description</label>
-          <input type="text" placeholder="Enter description for transaction" />
+          <input
+            type="text"
+            value={text}
+            onChange={(event) => setText(event.target.value)}
+            placeholder="Enter description for transaction"
+          />
         </div>
         <div className="form-control">
           <label htmlFor="amount">Amount* </label>
-          <input type="number" placeholder="Enter amount of transaction" />
+          <input
+            type="number"
+            value={amount}
+            onChange={(event) => setAmount(event.target.value)}
+            placeholder="Enter amount of transaction"
+          />
         </div>
         <button className="btn">Add Transaction</button>
       </form>
