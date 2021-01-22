5ab6eccd755a5fa2142afb286b8dc8732bb3b3eb
Romie Hecdivert <65568062+rh9891@users.noreply.github.com>
2021-01-22 13:07:44 -0500

    Added functionality to delete transactions that also automatically updates the balance, income, and expenses.

--- src/context/AppReducer.js
+++ src/context/AppReducer.js
@@ -1,6 +1,13 @@
 // Specifies the application's state changes in response to certain actions to our context.
 export default (state, action) => {
   switch (action.type) {
+    case "DELETE_TRANSACTION":
+      return {
+        ...state,
+        transactions: state.transactions.filter(
+          (transaction) => transaction.id !== action.payload
+        ),
+      };
     default:
       return state;
   }
