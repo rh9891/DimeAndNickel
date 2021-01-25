73e60dcbbf4f235e983b39aced68494e4373b9a4
Romie Hecdivert <65568062+rh9891@users.noreply.github.com>
2021-01-25 17:29:21 -0500

    GET request successfully made on backend.

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -1,20 +1,35 @@
+const Transaction = require("../models/Transaction");
+
 // @description Gets all transactions.
 // @route GET request to /api/v1/transactions.
 // @access Public route.
-exports.getTransactions = (req, res, next) => {
-  res.send("GET transactions");
+exports.getTransactions = async (req, res, next) => {
+  try {
+    const transactions = await Transaction.find();
+
+    return res.status(200).json({
+      success: true,
+      count: transactions.length,
+      data: transactions,
+    });
+  } catch (error) {
+    return res.send(500).json({
+      success: false,
+      error: "An error has occurred.",
+    });
+  }
 };
 
 // @description Adds a transaction.
 // @route POST request to /api/v1/transactions.
 // @access Public route.
-exports.addTransaction = (req, res, next) => {
+exports.addTransaction = async (req, res, next) => {
   res.send("POST transaction");
 };
 
 // @description Deletes a transaction.
 // @route DELETE request to /api/v1/transactions/:id.
 // @access Public route.
-exports.deleteTransaction = (req, res, next) => {
+exports.deleteTransaction = async (req, res, next) => {
   res.send("DELETE transaction");
 };
