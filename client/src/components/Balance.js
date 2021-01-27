c4c47142c112ae006f7e68cfb0982f72de60c3c3
Romie Hecdivert <65568062+rh9891@users.noreply.github.com>
2021-01-27 14:19:00 -0500

    Imported a regex called numberWithCommas to amend the syntax of the amounts of the application.

--- client/src/components/Balance.js
+++ client/src/components/Balance.js
@@ -1,5 +1,6 @@
 import React, { useContext } from "react";
 import { GlobalContext } from "../context/GlobalState";
+import { numberWithCommas } from "../utils/format";
 
 export const Balance = () => {
   const { transactions } = useContext(GlobalContext);
@@ -10,7 +11,7 @@ export const Balance = () => {
   return (
     <>
       <h4 className="subtitle-balance">Current Balance</h4>
-      <h1 className="balance">${total}</h1>
+      <h1 className="balance">${numberWithCommas(total)}</h1>
     </>
   );
 };
